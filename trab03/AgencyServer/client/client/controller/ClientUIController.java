0cac0db79d9effc10ba256f98227659f247aabee
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2018-10-12 15:16:09 -0300

    Protótipo de interface

---
+++ trab03/AgencyServer/client/client/controller/ClientUIController.java
@@ -0,0 +1,63 @@
+package client.controller;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleGroup;
+import model.cidade.Cidade;
+import model.voo.InfoVoo;
+
+public class ClientUIController {
+    private ObservableList<InfoVoo> olInfoVoo = FXCollections.observableArrayList();
+
+    @FXML
+    private ToggleGroup tipoVoo;
+
+    @FXML
+    private ChoiceBox<Cidade> choiceOrigemVoo;
+
+    @FXML
+    private ChoiceBox<Cidade> choiceDestinoVoo;
+
+    @FXML
+    private DatePicker dateVooIda;
+
+    @FXML
+    private DatePicker dateVooVolta;
+
+    @FXML
+    private Spinner<Integer> spinnerNumPessoasVoo;
+
+    @FXML
+    private Button buttonConsultarVoo;
+
+    @FXML
+    private TableView<InfoVoo> tableVooIda;
+
+    @FXML
+    private TableView<InfoVoo> tableVooVolta;
+
+    @FXML
+    private TableColumn<InfoVoo, Number> columnIdVooIda;
+
+    @FXML
+    public void initialize() {
+        // Inicializa as ChoiceBox com as cidades do enum Cidade
+        choiceOrigemVoo.getItems().setAll(Cidade.values());
+        choiceDestinoVoo.getItems().setAll(Cidade.values());
+
+        // Inicializa o seletor de número de pessoas
+        spinnerNumPessoasVoo.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 8, 1));
+
+        // Configura as colunas
+        columnIdVooIda.setCellValueFactory(item -> new SimpleIntegerProperty(item.getValue().getId()));
+    }
+}
